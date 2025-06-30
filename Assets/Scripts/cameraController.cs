22b01b20bef087b26443304d52225affb142b76d
Veragath <werwol@onet.eu>
2025-06-30 21:20:08 +0200

    Assign points to virtual camera and tab is working

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -5,14 +5,6 @@ using Unity.Cinemachine;
 
 public class cameraController : MonoBehaviour
 {
-
-    internal enum updateMethod{
-        fixedUpdate,
-        update,
-        lateUptade
-    }
-    [SerializeField]private updateMethod updateDemo;
-
     private GameObject attachedVehicle;
     private CinemachineVirtualCamera virtualCam;
 
@@ -21,28 +13,52 @@ public class cameraController : MonoBehaviour
 
     public int locationIndicator = 2;
 
-    private void Start()
+    // Publiczna metoda do przypisania pojazdu z GameManagera
+    public void AssignCameraToVehicle(GameObject vehicle)
     {
-        attachedVehicle = GameObject.FindGameObjectWithTag("Player");
+        attachedVehicle = vehicle;
         virtualCam = attachedVehicle.GetComponentInChildren<CinemachineVirtualCamera>();
+
+        if (attachedVehicle.transform.Find("CAMERA") == null)
+        {
+            Debug.LogError("CAMERA folder nie znaleziony w pojeÅºdzie!");
+            return;
+        }
 
         cameraPositionFolder = attachedVehicle.transform.Find("CAMERA").gameObject;
         camLocations = cameraPositionFolder.GetComponentsInChildren<Transform>();
 
         if (virtualCam != null)
         {
+            Debug.Log("Virtual Camera found: " + virtualCam.name);
             virtualCam.Priority = 20;
             virtualCam.Follow = camLocations[locationIndicator];
             virtualCam.LookAt = camLocations[1];
+
+            Debug.Log("Camera locations initialized. Current Follow: " + camLocations[locationIndicator].name);
+        }
+        else
+        {
+            Debug.LogError("No Virtual Camera found in the attached vehicle.");
         }
     }
-    private void Update() {
+
+    private void Update()
+    {
+        if (attachedVehicle == null || virtualCam == null || camLocations == null) return;
+
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             if (locationIndicator >= 4 || locationIndicator < 2)
+            {
                 locationIndicator = 2;
+                Debug.Log("Location Indicator: " + locationIndicator + " camlocations: " + camLocations[locationIndicator].name);
+            }
             else
+            {
                 locationIndicator++;
+                Debug.Log("else Location Indicator: " + locationIndicator);
+            }
 
             virtualCam.Follow = camLocations[locationIndicator];
         }
